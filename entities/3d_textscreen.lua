b0dc2cab924105dbc1440fef765b3855ddf31302
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2020-12-13 03:53:57 +1000

    Scaffolding

---
+++ entities/3d_textscreen.lua
@@ -0,0 +1,12 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_entity"
+ENT.PrintName = "3D Text Sign"
+ENT.Author = "Fluffy Servers"
+
+ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
+
+function ENT:Initialize()
+
+end
---
+++ lua/weapons/gmod_tool/stools/3dtext.lua
@@ -0,0 +1,12 @@
+TOOL.Category = "Construction"
+TOOL.Name = "#tool.3dtext.name"
+
+cleanup.Register("3dtext")
+
+function TOOL:LeftClick(tr)
+
+end
+
+function TOOL:RightClick(tr)
+
+end
