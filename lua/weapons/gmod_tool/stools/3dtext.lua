4ec75621a9de6910d5358e32c07c3a9afe3ba11a
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2020-12-13 19:48:44 +1000

    Customise text in toolgun

--- lua/weapons/gmod_tool/stools/3dtext.lua
+++ lua/weapons/gmod_tool/stools/3dtext.lua
@@ -3,6 +3,8 @@ TOOL.Name = "#tool.3dtext.name"
 
 cleanup.Register("3dtext")
 
+TOOL.ClientConVar["text"] = "Cool Text"
+
 function TOOL:LeftClick(tr)
     if CLIENT then return true end
 
@@ -15,9 +17,24 @@ function TOOL:LeftClick(tr)
     sign:Spawn()
     sign:Activate()
 
+    sign:SetNWString("SignText", self:GetClientInfo("text") or "Cool Text")
     return true
 end
 
 function TOOL:RightClick(tr)
 
+end
+
+function TOOL.BuildCPanel(panel)
+    panel:AddControl("Header", {
+        Text = "#tool.3dtext.name",
+        Description = "#tool.textscreen.desc"
+    })
+
+    local textbox = vgui.Create("DTextEntry")
+    textbox:SetUpdateOnType(true)
+    textbox:SetEnterAllowed(true)
+    textbox:SetConVar("3dtext_text")
+    textbox:SetValue(GetConVar("3dtext_text"):GetString())
+    panel:AddItem(textbox)
 end
